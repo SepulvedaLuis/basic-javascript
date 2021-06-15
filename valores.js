3ce17fbe0679fa53f8f5bc4d216b615b891a9adb
Luis Sepulveda <sepulvedaluis022@gmail.com>
2021-06-14 19:34:33 -0500

    Tipos de valores y como definir variables JS

---
+++ valores.js
@@ -0,0 +1,19 @@
+// número 40
+
+// string "Luis de Granda"
+
+// valores booleanos   true --> 1 and false --> 0
+
+// manejamos otros valores como null y undefined los cuales son también llamados
+//valores primitivos
+
+// los arreglos o arrays son valores primitivos que se organizan [1,2,3] 
+// y son tipo objeto
+
+//también contamos con otro valor tipo objeto {nombre : "Luis"} también llamado 
+//diccionario en otros lenguajes
+
+//para saber el tipo de un valor podemos utilizar typeof valor y podremos saber
+//en pocas palabras qde que tipo es el valor, también puede mostrar undefined lo que
+// indica que el valor no esta definido
+
---
+++ variables.js
@@ -0,0 +1,18 @@
+/*Vamos a ver las formas de definir una variable y la forma correcta de definirlas
+se utiliza la palabra reservada var para indicar que vamos a definir una variable,
+es importante presente que al declarar la variable lo que hacemos es revervar un
+espacio en memoria para nuestro uso*/
+
+var nombre_variable = "valor de la variable";  
+
+//toda línea de código debe ser finalizada con ;
+
+//vamos a declarar una varible
+var edad;
+//vamos a inicializar la variable brindadole un valor
+edad = 21;
+//declaramos un array y lo inicializamos a la vez
+var elementos = ["Computadora", "Celular"];
+//generamos un object el cual guardamos en persona 
+var persona = { nombre : "Luis" , edad: 21}; 
+

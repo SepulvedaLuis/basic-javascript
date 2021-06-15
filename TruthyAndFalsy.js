ec7e409718ddeb53326024ecaececc8e80a7088b
Luis Sepulveda <sepulvedaluis022@gmail.com>
2021-06-14 21:17:43 -0500

    Definición y ejemplos de Truthy and Falsy

---
+++ TruthyAndFalsy.js
@@ -0,0 +1,16 @@
+//Ejemplos en los que Boolean devuelve Falso: Falsy
+Boolean(0); //false
+Boolean(null); //false
+Boolean(NaN); //false
+Boolean(undefined); //false
+Boolean(false); //false
+Boolean(""); //false
+
+//Ejemplos en los que Boolean devuelve verdadero: Thruthy
+Boolean(1); //true para 1 o cualquier número diferente de cero (0)
+Boolean("a"); //true para cualquier caracter o espacio en blanco en el string
+Boolean([]); //true aunque el array esté vacío
+Boolean({}); //true aunque el objeto esté vacío
+Boolean(function(){}); //Cualquier función es verdadera también
+Boolean("False") // true 
+Boolean(-1) // true ya que l número que da negativo es (0)

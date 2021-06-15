c473ecfc67eca5272e54fbac2c65a11b746b45c6
Luis Sepulveda <sepulvedaluis022@gmail.com>
2021-06-14 21:26:38 -0500

    Definición y ejemplos de los Operadores JS

---
+++ operadores.js
@@ -0,0 +1,57 @@
+/*Operador de asignacion
+Simbolo	Descripcion
+=	operador de asignacion
+Operadores de comparacion
+Simbolo	Descripcion
+==	igual que
+===	estrictamente igual que
+> or >= or >==	mayor o mayor igual que
+< or <= or <==	menor o menor igual que
+!= or !==	diferente que
+Operadores aritmeticos
+Simbolo	Descripcion
++	operador suma este se utiliza para concatener dos cadenas de texto.
+-	operador resta
+*	operador de multicacion
+/	operador de division
+%	operador de modulo
+**	operador de potenciacion
+tambien se les conoce como operadores binarios. por que toman dos valores y generan un resultado.
+
+Operadores logicos
+Simbolo	Descripcion
+!	NOT niega un valor
+&&	AND
+||	OR*/
+
+//Operadores binarios:
+3 + 2 //Suma
+50 - 10 // Resta
+10 * 20 //Multiplicación
+20 / 2 //División
+
+"Diego " + "De Granda" //concatenación de strings
+
+//Operadores unitarios:
+!false //negación de la negación = true
+
+//Operadores de asignación:
+var a = 1; //Asignamos un valor a la variable
+
+//Operadores para comparar:
+3 == "3"; //Compara los valores y devuelve "true" en este caso
+
+3 === "3"; //Compara y valida los tipos y valores. Devuelve "falso" en este caso
+
+5 < 3 //Compara y valida si el 5 es menor a 3
+5 > 3 //Compara y valida si el 5 es mayor a 3
+5 <= 6 //Compara y valida si el 5 es menor o igual al 6
+5 >= 6 //Compara y valida si el 5 es mayor o igual al 6
+
+a && b //Valida si ambas variables son verdaderas para que se cumpla la condición
+a || b //Aquí se cumple la condición si alguna de las dos variables es verdadera
+
+var edad = 40
+edad++ //Incrementa el valor en 1
+
+edad += 2 //Incrementa el valor por 2
